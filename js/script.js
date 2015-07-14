751477a32d2a54d60b5c5ddb792bf675d879f477
vagrant <vagrant@precise32.(none)>
2015-07-14 01:06:24 +0000

    moved js to seperate file

--- js/script.js
+++ js/script.js
@@ -1,14 +1,41 @@
-// Initially disable the button
-$("button").prop("disabled", true);
+var TweetBox = React.createClass({
+	getInitialState: function() {
+		return {
+			text: ""
+		}
+	},
+	handleChange: function(event) {
+		this.setState({ text: event.target.value })
+	},
+  	render: function() {
+	    return (
+	      <div className="well clearfix">
+	        <textarea className="form-control" onChange={this.handleChange}></textarea>
+	        <br/>
+	        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
 
-// When the value of the text area changes...
-$("textarea").on("input", function() {
-  // If there's at least one character...
-  if ($(this).val().length > 0) {
-    // Enable the button.
-    $("button").prop("disabled", false);
-  } else {
-    // Else, disable the button.
-    $("button").prop("disabled", true);
+	      </div>
+	    );
   }
 });
+
+React.render(
+  <TweetBox />,
+  document.getElementById('tweet')
+);
+
+
+// // Initially disable the button
+// $("button").prop("disabled", true);
+
+// // When the value of the text area changes...
+// $("textarea").on("input", function() {
+//   // If there's at least one character...
+//   if ($(this).val().length > 0) {
+//     // Enable the button.
+//     $("button").prop("disabled", false);
+//   } else {
+//     // Else, disable the button.
+//     $("button").prop("disabled", true);
+//   }
+// });
