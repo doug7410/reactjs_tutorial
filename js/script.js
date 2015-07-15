5a27ae4b59397424e6496fe654250ae752d3b8b8
vagrant <vagrant@precise32.(none)>
2015-07-15 02:29:36 +0000

    fix indentation

--- js/script.js
+++ js/script.js
@@ -1,68 +1,68 @@
 var TweetBox = React.createClass({
-	getInitialState: function() {
-		return {
-			text: "",
-			photoAdded: false
-		}
-	},
-	handleChange: function(event) {
-		this.setState({ text: event.target.value })
-	},
-	togglePhoto: function(event) {
-	  this.setState({ photoAdded: !this.state.photoAdded });
-	},
-	remainingCharacters: function() {
-	  if (this.state.photoAdded) {
-	    return 140 - 23 - this.state.text.length;
-	  } else {
-	    return 140 - this.state.text.length;
-	  }
-	},
+  getInitialState: function() {
+    return {
+      text: "",
+      photoAdded: false
+    }
+  },
+  handleChange: function(event) {
+    this.setState({ text: event.target.value })
+  },
+  togglePhoto: function(event) {
+    this.setState({ photoAdded: !this.state.photoAdded });
+  },
+  remainingCharacters: function() {
+    if (this.state.photoAdded) {
+      return 140 - 23 - this.state.text.length;
+    } else {
+      return 140 - this.state.text.length;
+    }
+  },
   overflowAlert: function() {
     if (this.remainingCharacters() < 0) {
-    	if (this.state.photoAdded) {
-			  var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
-			  var overflowText = this.state.text.substring(140 - 23);
-			} else {
-			  var beforeOverflowText = this.state.text.substring(140 - 10, 140);
-			  var overflowText = this.state.text.substring(140);
-			}
+      if (this.state.photoAdded) {
+        var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        var overflowText = this.state.text.substring(140 - 23);
+      } else {
+        var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        var overflowText = this.state.text.substring(140);
+      }
       
       return (
         <div className="alert alert-warning">
           <strong>Oops! Too Long:</strong>
           &nbsp;...{beforeOverflowText}
-      		<strong className="bg-danger">{overflowText}</strong>
+          <strong className="bg-danger">{overflowText}</strong>
         </div>
       );
     } else {
       return "";
     }
   },
-  	render: function() {
-  		if(this.remainingCharacters() <= 0){
-  			var charCountClass = "charCount text-danger";	
-  		} else {
-  			var charCountClass = "charCount";
-  		}
+  render: function() {
+    if(this.remainingCharacters() <= 0){
+      var charCountClass = "charCount text-danger"; 
+    } else {
+      var charCountClass = "charCount";
+    }
 
-	    return (
-	      <div className="well clearfix">
-          { this.overflowAlert() }
-	        <textarea className="form-control" 
-                    onChange={this.handleChange}></textarea>
-	        <br/>
-        	Remaning characters: 
-        	<span className={charCountClass}> { this.remainingCharacters() }</span>
-	        <button className="btn btn-primary pull-right" 
-            disabled={this.remainingCharacters() === 140}>Tweet</button>
-	        <button className="btn btn-default pull-right" 
-	        	onClick={this.togglePhoto}>
-			  {this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }
-			</button>
+    return (
+      <div className="well clearfix">
+        { this.overflowAlert() }
+        <textarea className="form-control" 
+                  onChange={this.handleChange}></textarea>
+        <br/>
+        Remaning characters: 
+        <span className={charCountClass}> { this.remainingCharacters() }</span>
+        <button className="btn btn-primary pull-right" 
+          disabled={this.remainingCharacters() === 140}>Tweet</button>
+        <button className="btn btn-default pull-right" 
+          onClick={this.togglePhoto}>
+          {this.state.photoAdded ? "✓ Photo Added" : "Add Photo" }
+        </button>
 
-	      </div>
-	    );
+      </div>
+    );
   }
 });
 
